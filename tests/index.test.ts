4917c5e218ea298a7ae451e7c05d808236e34427
Colin Brooks <gin9err4ver@gmail.com>
2026-07-21 00:49:05 +0100

    Add Vitest testing framework

---
+++ tests/index.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test } from "vitest";
+
+describe("Job Tracker", () => {
+  test("should confirm the test environment is working", () => {
+    expect(true).toBe(true);
+  });
+});
